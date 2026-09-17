b0cb0d5416f9d3b29fdd960c14b9697e32a84250
Rajeev Suravajhula <rajeev.suravajhula@gmail.com>
2026-09-16 23:23:38 -0400

    added the initial set up for the project

---
+++ dbt/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-09-17T03:16:38.659501+00:00
+-- finished_at: 2026-09-17T03:16:38.755668+00:00
+-- elapsed: 96ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c71f64-0001-c7b2-0006-0e070009cd02
+-- desc: not provided
+select 1 as id;
