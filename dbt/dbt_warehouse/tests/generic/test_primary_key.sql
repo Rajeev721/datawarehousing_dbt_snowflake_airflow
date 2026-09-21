3b0799ce3687c3357c1b82621867f2b9ff6cd179
Rajeev Suravajhula <rajeev.suravajhula@gmail.com>
2026-09-21 15:05:40 -0400

    added the raw customer model

---
+++ dbt/dbt_warehouse/tests/generic/test_primary_key.sql
@@ -0,0 +1,12 @@
+{% test primary_key_validation(model, column_name) %}
+
+    select
+        {{ column_name }},
+            count(*)
+    from
+        {{ model }}
+    group by
+        {{ column_name }}
+    having count(*) > 1
+
+    {% endtest %}
